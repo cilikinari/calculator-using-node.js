498ab2491042392d9b342a546419cf7f4365c765
cilikinari <cilikinari2005@gmail.com>
2025-09-20 23:30:19 +0800

    adding the functions to operating

---
+++ calculator.js
@@ -0,0 +1,17 @@
+function calculator(operator, num1, num2) {
+  let hasil;
+
+  if (operator === "+") {
+    hasil = num1 + num2;
+  } else if (operator === "-") {
+    hasil = num1 - num2;
+  } else if (operator === "*") {
+    hasil = num1 * num2;
+  } else if (operator === "/") {
+    hasil = num2 !== 0 ? num1 / num2 : "angka tidak bisa dibagi 0";
+  } else {
+    hasil = "operator tidak ditemukan";
+  }
+
+  return hasil;
+}
